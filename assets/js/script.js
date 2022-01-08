34c4cee04376bb6cdc506c945989eba496dd9a0f
Zack Greenfield <zackg007@verizon.net>
2022-01-08 16:08:57 -0500

    removed dropdown menu, left navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,24 +32,8 @@ var formSubmitHandler = function(event) {
     }
 };
 
-// dropdown functionality
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
 
 
 
